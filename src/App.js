15298c84d427fb7d5c5fdc45444ea7134e24778f
anassigit <anassigit1998@gmail.com>
2023-10-25 19:38:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="wrapper">
+        <h1>Bubbles Animation</h1>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+        <div><span className="dot"></span></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
