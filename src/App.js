e3e59fe39753e0164b5e5e37cb46a7f9fa6bde26
anassigit <anassigit1998@gmail.com>
2023-11-02 00:24:40 +0700

    commmit final

--- src/App.js
+++ src/App.js
@@ -18,8 +18,36 @@ import sepuluh from './Words For Her/Aku Sangat Sayang Sama Kamu.svg'
 import sebelas from './Words For Her/I Present This To You.svg'
 import duabelas from './Words For Her/For Ranti.svg'
 
-import firstOneGroup from './pics/group1-1.jpg'
+import firstFstGroup from './pics/group1-1.jpg'
 import firstSecGroup from './pics/group1-2.jpg'
+import firstThdGroup from './pics/group1-3.jpg'
+
+import secondFstGroup from './pics/group2-1.jpg'
+import secondSecGroup from './pics/group2-2.jpg'
+
+import thirdFstGroup from './pics/group3-1.jpg'
+import thirdSecGroup from './pics/group3-2.jpg'
+import thirdThdGroup from './pics/group3-3.jpg'
+
+import fourthFstGroup from './pics/group4-1.jpg'
+import fourthSecGroup from './pics/group4-2.jpg'
+import fourthThdGroup from './pics/group4-3.jpg'
+
+import fifthFstGroup from './pics/group5-1.jpg'
+import fifthSecGroup from './pics/group5-2.jpg'
+import fifthThdGroup from './pics/group5-3.jpg'
+
+import sixthFstGroup from './pics/group6-1.jpg'
+import sixthSecGroup from './pics/group6-2.jpg'
+import sixthThdGroup from './pics/group6-4.jpg'
+
+import specialFstGroup from './pics/group-special2.jpg'
+import specialSecGroup from './pics/group-special4.jpg'
+import specialThdGroup from './pics/group-special3.jpg'
+import specialFthGroup from './pics/group-special-tidur2.jpg'
+
+import beforeLast from './Words For Her/Liat Semesra Apa Kita.svg'
+import lastLast from './Words For Her/I Love You Ranti.svg'
 
 function App() {
 
@@ -49,6 +77,25 @@ function App() {
       }, 4000);
     }
   }, [playButton]);
+
+  useEffect(() => {
+    if (buttonVisible > 11) {
+      const delay = picsStart === 2 ? 8000 : 9000; // Adjust the delay based on picStart value
+      const interval = setInterval(() => {
+        setPicStart((prevPicStart) => {
+          if (prevPicStart < 12) {
+            return prevPicStart + 1;
+          } else {
+            clearInterval(interval);
+            return prevPicStart;
+          }
+        });
+      }, delay);
+    }
+  }, [buttonVisible]);
+
+
+  console.log(picsStart)
 
   useEffect(() => {
     const playAudio = () => {
@@ -149,47 +196,272 @@ function App() {
                   src={duabelas}
                 />
               )}
-            </div>
+              {
+                buttonVisible > 11 && picsStart === 0 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={firstFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={firstSecGroup}
+                    />
+                    <img
+                      className='group-pics-3'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={firstThdGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 1 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={secondFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={secondSecGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 2 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={thirdFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={thirdSecGroup}
+                    />
+                  </div>
+                )
+              }
 
+              {
+                buttonVisible > 11 && picsStart === 3 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={fourthFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={fourthSecGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 4 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={fifthFstGroup}
+                    />
+                    <img
+                      className='group-pics-3'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={fifthThdGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 5 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={sixthFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={sixthSecGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 6 && (
+                  <img
+                    id='last-text'
+                    src={beforeLast}
+                  />
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 7 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={specialFstGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={specialSecGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 8 && (
+                  <div
+                    id='photo-container one'
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "3vw",
+                    }}
+                  >
+                    <img
+                      className='group-pics-1'
+                      style={{
+                        width: "40vh"
+                      }}
+                      src={specialFthGroup}
+                    />
+                    <img
+                      className='group-pics-2'
+                      style={{
+                        height: "40vh"
+                      }}
+                      src={specialThdGroup}
+                    />
+                  </div>
+                )
+              }
+              {
+                buttonVisible > 11 && picsStart === 9 && (
+                  <img
+                    id='last-text'
+                    src={lastLast}
+                  />
+                )
+              }
+            </div>
           )
           :
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: "3vw",
-            }}
+          <button
+            className='fade-in-btn text-light'
+            style={{ zIndex: 2, fontSize: '4vw', padding: "3vh 3vw 3vh 3vw" }}
+            onClick={() => setPlayButton(true)}
           >
-            <img
-              className='group-pics-1'
-              style={{
-                width: "15vw",
-              }}
-              src={firstOneGroup}
-            />
-            <img
-              className='group-pics-2'
-              style={{
-                width: "15vw",
-              }}
-              src={firstSecGroup}
-            />
-            <img
-              className='group-pics-3'
-              style={{
-                width: "15vw",
-              }}
-              src={firstSecGroup}
-            />
-          </div>
-        // <button
-        //   className='fade-in-btn text-light'
-        //   style={{ zIndex: 2, fontSize: '4vw', padding: "3vh 3vw 3vh 3vw" }}
-        //   onClick={() => setPlayButton(true)}
-        // >
-        //   click ini kalo udah nyalain suaranya :)
-        // </button>
+            click ini kalo udah nyalain suaranya :)
+          </button>
       }
       <div className="stars" style={{ zIndex: -999 }}>
         <div className="star"></div>
